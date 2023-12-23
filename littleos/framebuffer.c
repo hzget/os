f6da3f0869eacec80541004006cd7965d425f17f
hzget <penghuaizhen@163.com>
2023-12-23 15:38:28 +0800

    hide details in framebuffer

--- littleos/framebuffer.c
+++ littleos/framebuffer.c
@@ -1,5 +1,40 @@
 #include "io.h"
-#include "framebuffer.h"
+
+/* The I/O ports */
+#define FB_COMMAND_PORT 0x3D4
+#define FB_DATA_PORT 0x3D5
+
+/* The I/O port commands */
+#define FB_HIGH_BYTE_COMMAND 14
+#define FB_LOW_BYTE_COMMAND 15
+
+#define FB_GREEN 2
+#define FB_DARK_GREY 8
+
+#define FRAMEBUFFER_ADDRESS 0x000B8000
+
+/** fb_move_cursor:
+ *  Moves the cursor of the framebuffer to the given position
+ *
+ *  @param pos The new position of the cursor
+ */
+void fb_move_cursor(unsigned short pos);
+
+/** fb_write_cell:
+ *  Writes a character with the given foreground and background to position i
+ *  in the framebuffer.
+ *
+ *  @param i  The location in the framebuffer
+ *  @param c  The character
+ *  @param fg The foreground color
+ *  @param bg The background color
+ */
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
+
+int fb_write_str(char *buf, unsigned int len);
+
+
+
 
 unsigned short __fb_present_pos = 0x0;
 
--- littleos/kmain.c
+++ littleos/kmain.c
@@ -4,7 +4,6 @@
 char welcome[] = "Little OS";
 
 void kmain() {
-    fb_move_cursor(80*7);
-    fb_write_str(welcome, sizeof(welcome));
+    write(welcome, sizeof(welcome));
 }
 
