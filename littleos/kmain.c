b86145fb79428a82fc87370bbbf80e4d18c297c2
hzget <penghuaizhen@163.com>
2024-01-03 14:59:19 +0800

    support printf function

--- littleos/kmain.c
+++ littleos/kmain.c
@@ -4,11 +4,13 @@
 #include "interrupts.h"
 #include "segments.h"
 #include "serial.h"
+#include "stdio.h"
 
 char welcome[] = "Little OS";
 
 void kmain() {
     fb_write(welcome, sizeof(welcome));
+    printf("\n%s\n", "hello os");
     serial_write(welcome, sizeof(welcome));
     segments_install_gdt();
     interrupts_install_idt();
