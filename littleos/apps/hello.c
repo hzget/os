0ad292765b0819f0ef8cd29173741edf90adbfdb
hzget <penghuaizhen@163.com>
2024-01-05 20:05:07 +0800

    update makefile; comment hlt code in modules; update print string

--- littleos/apps/hello.c
+++ littleos/apps/hello.c
@@ -2,7 +2,7 @@
 void hello() {
     unsigned int magic = 0x1234ABCD;
     __asm__ volatile("mov %0, %%eax" ::"r"(magic));
-    __asm__ volatile("hlt;");
+//    __asm__ volatile("hlt;");
 }
 int main(void) {
     hello();
