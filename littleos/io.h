55d634eeaaf232c31060cdb197f002f1d2e56113
hzget <penghuaizhen@163.com>
2024-02-07 16:07:30 +0800

    update io files

--- littleos/io.h
+++ littleos/io.h
@@ -3,13 +3,16 @@
 #ifndef IO_H
 #define IO_H
 
+#include <stdint.h>
+
 /** outb:
  *  Sends the given data to the given I/O port. Defined in io.s
  *
  *  @param port The I/O port to send the data to
  *  @param data The data to send to the I/O port
  */
-void outb(unsigned short port, unsigned char data);
+void outb(uint16_t port, uint8_t data);
+void outw(uint16_t port, uint16_t data);
 
 /** inb:
  *  Read a byte from an I/O port.
@@ -17,6 +20,7 @@ void outb(unsigned short port, unsigned char data);
  *  @param  port The address of the I/O port
  *  @return      The read byte
  */
-unsigned char inb(unsigned short port);
+uint8_t inb(uint16_t port);
+uint16_t inw(uint16_t port);
 
 #endif /* IO_H */
