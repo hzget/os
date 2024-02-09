73fd506ad8c2efed4959def9cd032c41e8eb7820
hzget <penghuaizhen@163.com>
2024-02-09 21:16:32 +0800

    harddisk supports filesystem fat16

---
+++ littleos/harddisk/create_harddisk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# create a harddisk with a filesystem that supports FAT16
+# and copy a txt file to it
+
+rm -rf boot.bin harddisk.img
+nasm -f bin boot.asm -o boot.bin
+dd if=boot.bin >> harddisk.img
+dd if=/dev/zero bs=1048576 count=16 >> harddisk.img
+sudo mount -t vfat harddisk.img /mnt
+sudo cp hello.txt /mnt/
+sudo umount /mnt
