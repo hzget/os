6f6201a230d369d8f46f5502665e330d5b1893f6
hzget <penghuaizhen@163.com>
2024-03-04 00:06:01 +0800

    fix issue strnlen not pass unit test

--- littleos/test/string_test.c
+++ littleos/test/string_test.c
@@ -15,9 +15,21 @@ static void test_strlen(void **state) {
     //   assert_true(0 == strlen(str));
 }
 
+static void test_strnlen(void **state) {
+    (void)state; /* unused */
+    assert_true(5 == strnlen("hello", 6));
+    assert_true(5 == strnlen("hello", 5));
+    assert_true(4 == strnlen("hello", 4));
+    assert_true(0 == strnlen("hello", 0));
+    assert_true(0 == strnlen("", 5));
+    assert_true(0 == strnlen("", 0));
+    //    assert_true(0 == strnlen("", -1));
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_strlen),
+        cmocka_unit_test(test_strnlen),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
